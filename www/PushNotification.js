721cae127082fa740e5833f45972c0ed412ca98f
André <andre@andre.epdev.de>
2013-09-23 11:15:52 +0200

    phonegap 3 compatible

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -7,57 +7,46 @@
 // Copyright 2012 Olivier Louvignes. All rights reserved.
 // MIT Licensed
 
-(function(cordova) {
+var exec = require('cordova/exec');
 
-	function PushNotification() {}
-
-	// Call this to register for push notifications and retreive a deviceToken
-	PushNotification.prototype.registerDevice = function(config, callback) {
-		cordova.exec(callback, callback, "PushNotification", "registerDevice", config ? [config] : []);
-	};
-
-	// Call this to retreive pending notification received while the application is in background or at launch
-	PushNotification.prototype.getPendingNotifications = function(callback) {
-		cordova.exec(callback, callback, "PushNotification", "getPendingNotifications", []);
-	};
-
-	// Call this to get a detailed status of remoteNotifications
-	PushNotification.prototype.getRemoteNotificationStatus = function(callback) {
-		cordova.exec(callback, callback, "PushNotification", "getRemoteNotificationStatus", []);
-	};
-
-  // Call this to get the current value of the application badge number
-  PushNotification.prototype.getApplicationIconBadgeNumber = function(callback) {
-    cordova.exec(callback, callback, "PushNotification", "getApplicationIconBadgeNumber", []);
-  };
-
-	// Call this to set the application icon badge
-	PushNotification.prototype.setApplicationIconBadgeNumber = function(badge, callback) {
-		cordova.exec(callback, callback, "PushNotification", "setApplicationIconBadgeNumber", [{badge: badge}]);
-	};
-
-	// Call this to clear all notifications from the notification center
-	PushNotification.prototype.cancelAllLocalNotifications = function(callback) {
-		cordova.exec(callback, callback, "PushNotification", "cancelAllLocalNotifications", []);
-	};
-
-	// Call this to retreive the original device unique id
-	// @warning As of today, usage is deprecated and requires explicit consent from the user
-	PushNotification.prototype.getDeviceUniqueIdentifier = function(callback) {
-		cordova.exec(callback, callback, "PushNotification", "getDeviceUniqueIdentifier", []);
-	};
-
-	// Event spawned when a notification is received while the application is active
-	PushNotification.prototype.notificationCallback = function(notification) {
+var PushNotification = {
+// Call this to register for push notifications and retreive a deviceToken
+	registerDevice: function(config, callback) {
+		exec(callback, callback, "PushNotification", "registerDevice", config ? [config] : []);
+	},
+// Call this to retreive pending notification received while the application is in background or at launch
+	getPendingNotifications: function(callback) {
+		exec(callback, callback, "PushNotification", "getPendingNotifications", []);
+	},
+// Call this to get a detailed status of remoteNotifications
+	getRemoteNotificationStatus: function(callback) {
+		exec(callback, callback, "PushNotification", "getRemoteNotificationStatus", []);
+	},
+// Call this to get the current value of the application badge number
+	getApplicationIconBadgeNumber: function(callback) {
+		exec(callback, callback, "PushNotification", "getApplicationIconBadgeNumber", []);
+	},
+// Call this to set the application icon badge
+	setApplicationIconBadgeNumber: function(badge, callback) {
+		exec(callback, callback, "PushNotification", "setApplicationIconBadgeNumber", [{badge: badge}]);
+	},
+// Call this to clear all notifications from the notification center
+	cancelAllLocalNotifications: function(callback) {
+		exec(callback, callback, "PushNotification", "cancelAllLocalNotifications", []);
+	},
+// Call this to retreive the original device unique id
+// @warning As of today, usage is deprecated and requires explicit consent from the user
+	getDeviceUniqueIdentifier: function(callback) {
+		exec(callback, callback, "PushNotification", "getDeviceUniqueIdentifier", []);
+	},
+// Event spawned when a notification is received while the application is active
+	notificationCallback: function(notification) {
 		var ev = document.createEvent('HTMLEvents');
 		ev.notification = notification;
 		ev.initEvent('push-notification', true, true, arguments);
 		document.dispatchEvent(ev);
-	};
+	}
+}
 
-	cordova.addConstructor(function() {
-		if(!window.plugins) window.plugins = {};
-		window.plugins.pushNotification = new PushNotification();
-	});
+module.exports = PushNotification;
 
-})(window.cordova || window.Cordova);
